a6fd15a8e057064602a34d65cc69235be5b1ee34
mikaelbashir <bashirm-y16@mgs.org>
2023-10-30 11:40:47 +0000

    question 0, 1)i) completed

---
+++ run.py
@@ -0,0 +1,63 @@
+import time
+from autograder_term1week5 import *
+_globals = globals()
+
+
+# ============================
+# question 0
+# ============================
+
+
+# 5 Marks
+# Do not try to delete this cell
+# Run this cell for grading of Question 0
+_globals = globals()
+question0(_globals)
+
+
+# ============================
+# question 1)i)
+# ============================
+
+
+q1ia_answer = 17
+q1ib_answer = 64
+q1ic_answer = 2/3
+q1id_answer = 8
+q1ie_answer = 128
+q1if_answer = 32768
+# print(q1ia_answer)
+# print(q1ib_answer)
+# print(q1ic_answer)
+# print(q1id_answer)
+# print(q1ie_answer)
+# print(q1if_answer)
+
+'''
+# a)
+print(4**2 + 1)
+
+# b)
+print(4**(2+1))
+
+# c)
+print(2/3)
+
+# d)
+smallest = 0
+while True:
+    if smallest <= 381/47:
+        smallest += 1
+    else:
+        print(smallest - 1)
+        break
+
+# e)
+print(4 * 2**5)
+
+# f)
+print((4*2)**5)
+'''
+
+_globals = globals()
+question1(_globals)
