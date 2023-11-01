1a6a5aec41c3a8939c4aadfbc2bddc278be3dcda
mikaelbashir <bashirm-y16@mgs.org>
2023-11-01 08:52:02 +0000

    question 1)ii), 2)i) completed

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import time
 from autograder_term1week5 import *
+import math
+from datetime import datetime
 _globals = globals()
 
 
@@ -59,5 +61,94 @@ print(4 * 2**5)
 print((4*2)**5)
 '''
 
+
 _globals = globals()
 question1(_globals)
+
+
+# ============================
+# question 1)ii)
+# ============================
+
+
+q1iia_answer = 1237
+q1iib_answer = 1157
+q1iic_answer = 636
+start = time.time()
+q1iid_answer = pow(1417456, 1191761, 1820)
+duration = time.time() - start
+# print(q1iia_answer)
+# print(q1iib_answer)
+# print(q1iic_answer)
+# print(q1iid_answer)
+
+'''
+# a)
+print(372517%1820)
+
+# b)
+print((1417456+1191761)%1820)
+
+# c)
+print((1417456*1191761)%1820)
+
+# d)
+print(pow(1417456, 1191761, 1820))
+'''
+
+_globals = globals()
+question1_ii(_globals)
+
+
+# ============================
+# question 2)i)
+# ============================
+
+
+q2ia_answer = math.sqrt(50)
+q2ib_answer = math.cos(math.pi/7)
+q2ic_answer = math.e**(math.e**2)
+q2id_answer = math.log(58, math.e)
+q2ie_answer = math.log10(58)
+q2if_answer = math.pi/2 - math.atan(2)
+q2ig_answer = math.atanh(1/2)
+#print(q2ia_answer)
+#print(q2ib_answer)
+#print(q2ic_answer)
+#print(q2id_answer)
+#print(q2ie_answer)
+#print(q2if_answer)
+#print(q2ig_answer)
+
+'''
+# a)
+print(math.sqrt(50))
+
+# b)
+print(math.cos(math.pi/7))
+
+# c)
+print(math.e**(math.e**2))
+
+# d)
+print(math.log(58, math.e))
+
+# e)
+print(math.log10(58))
+
+# f)
+print(math.pi/2 - math.atan(2))
+
+# g)
+print(math.atanh(1/2))
+'''
+
+_globals = globals()
+question2_i(_globals)
+
+
+# ============================
+# question 2)i)
+# ============================
+
+
